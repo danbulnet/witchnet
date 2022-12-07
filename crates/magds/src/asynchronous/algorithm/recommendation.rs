a642345902919df2d7859fe35010861686d43a69
danbulnet <daniel@bulanda.net>
2022-12-07 22:21:10 +0100

    add recommend_filter fn

--- crates/magds/src/asynchronous/algorithm/recommendation.rs
+++ crates/magds/src/asynchronous/algorithm/recommendation.rs
@@ -1,24 +1,48 @@
+use std::sync::{ Arc, RwLock };
+
 use witchnet_common::{
-    data::{ DataTypeValue, DataCategory }
+    data::{ DataTypeValue, DataCategory },
+    neuron::NeuronAsync
 };
 
 use crate::asynchronous::magds::MAGDS;
 
+pub enum DataValueFilter {
+    Empty,
+    One(DataTypeValue),
+    Range((DataTypeValue, DataTypeValue)),
+    Many(Vec<DataTypeValue>)
+}
+
 pub fn recommend(
     magds: &mut MAGDS, 
-    features: &Vec<(u32, DataTypeValue)>,
+    features: &[(u32, DataTypeValue)],
     target: u32,
     fuzzy: bool
 ) -> Option<Vec<(DataTypeValue, f32)>> {
     let features: Vec<(u32, DataTypeValue, f32)> = features.into_iter()
         .map(|(id, value)| (*id, value.clone(), 1.0f32))
         .collect();
-    recommend_weighted(magds, &features, target, fuzzy)
+    recommend_weighted(magds, &features, &[], target, fuzzy)
+}
+
+pub fn recommend_filter(
+    magds: &mut MAGDS, 
+    features: &[(u32, DataTypeValue)],
+    filters: &[(u32, DataValueFilter)],
+    target: u32,
+    fuzzy: bool
+) -> Option<Vec<(DataTypeValue, f32)>> {
+    let features: Vec<(u32, DataTypeValue, f32)> = features.into_iter()
+        .map(|(id, value)| (*id, value.clone(), 1.0f32))
+        .collect();
+    recommend_weighted(magds, &features, filters, target, fuzzy)
 }
 
 pub fn recommend_weighted(
     magds: &mut MAGDS, 
     features: &Vec<(u32, DataTypeValue, f32)>,
+    filters: &[(u32, DataValueFilter)],
     target: u32,
     fuzzy: bool
 ) -> Option<Vec<(DataTypeValue, f32)>> {
@@ -66,6 +90,9 @@ pub fn recommend_weighted(
     if neurons_len == 0 { return None }
 
     let mut values_sorted: Vec<(DataTypeValue, f32)> = neurons.into_iter()
+        .filter(|neuron| {
+            if filters.is_empty() { true } else { filter_neuron(neuron, filters) }
+        })
         .map(
             |neuron| (
                 neuron.read().unwrap().explain_one(target), 
@@ -86,6 +113,26 @@ pub fn recommend_weighted(
     Some(values_sorted.into_iter().rev().collect())
 }
 
+fn filter_neuron(
+    neuron: &Arc<RwLock<dyn NeuronAsync>>, filters: &[(u32, DataValueFilter)]
+) -> bool {
+    let neuron = neuron.read().unwrap();
+    for (filter_id, filter) in filters {
+        if let Some(neuron_value) = &neuron.explain_one(*filter_id) {
+            let is_ok = match filter {
+                DataValueFilter::Empty => true,
+                DataValueFilter::One(value) => neuron_value == value,
+                DataValueFilter::Range((min, max)) => {
+                    neuron_value >= min && neuron_value <= max
+                }
+                DataValueFilter::Many(values) => values.contains(neuron_value),
+            };
+            if !is_ok { return false }
+        }
+    }
+    true
+}
+
 #[allow(unused_imports)]
 mod tests {
     use witchnet_common::data::DataTypeValue;
@@ -96,6 +143,8 @@ mod tests {
             algorithm::{ recommendation, prediction }
         }
     };
+
+    use super::*;
 
     #[test]
     fn recommend() {
@@ -127,6 +176,46 @@ mod tests {
         assert!(!recommendations.is_empty());
         assert!(recommendations.first().unwrap().1 > 0f32);
         println!("recommendations {:?}", recommendations);
+
+        let setosa_filters = vec![
+            (variety_sensor_id, DataValueFilter::One(Arc::<str>::from("setosa").into()))
+        ];
+        let recommendations = recommendation::recommend_filter(
+            &mut magds, &features, &setosa_filters, variety_sensor_id, true
+        ).unwrap();
+        assert!(recommendations.len() == 50);
+        assert!(recommendations.first().unwrap().1 > 0f32);
+        println!("recommendations filtered by setosa {:?}", recommendations);
+
+        let empty_filters = vec![(variety_sensor_id, DataValueFilter::Empty)];
+        let recommendations = recommendation::recommend_filter(
+            &mut magds, &features, &empty_filters, variety_sensor_id, true
+        ).unwrap();
+        assert!(recommendations.len() == 150);
+        assert!(recommendations.first().unwrap().1 > 0f32);
+        println!("recommendations filtered by empty filter {:?}", recommendations);
+
+        let range_filters = vec![
+            (sepal_length_sensor_id, DataValueFilter::Range((5.0.into(), 10.0.into())))
+        ];
+        let recommendations = recommendation::recommend_filter(
+            &mut magds, &features, &range_filters, variety_sensor_id, true
+        ).unwrap();
+        assert!(recommendations.len() == 128);
+        assert!(recommendations.first().unwrap().1 > 0f32);
+        println!("recommendations filtered by range filter {:?}", recommendations);
+
+        let many_filters = vec![(
+            variety_sensor_id, DataValueFilter::Many(
+                vec![Arc::<str>::from("setosa").into(), Arc::<str>::from("virginica").into()]
+            )
+        )];
+        let recommendations = recommendation::recommend_filter(
+            &mut magds, &features, &many_filters, variety_sensor_id, true
+        ).unwrap();
+        assert!(recommendations.len() == 100);
+        assert!(recommendations.first().unwrap().1 > 0f32);
+        println!("recommendations filtered by setosa and versicolor {:?}", recommendations);
         
         let pred = prediction::predict(&mut magds, &features, variety_sensor_id, true);
         println!("pred {:?}", pred);
--- crates/magds/src/synchronous/algorithm/recommendation.rs
+++ crates/magds/src/synchronous/algorithm/recommendation.rs
@@ -1,8 +1,18 @@
+use std::{ rc::Rc, cell::RefCell };
+
 use witchnet_common::{
-    data::{ DataTypeValue, DataCategory }
+    data::{ DataTypeValue, DataCategory },
+    neuron::Neuron
 };
 
 use crate::synchronous::magds::MAGDS;
+
+pub enum DataValueFilter {
+    Empty,
+    One(DataTypeValue),
+    Range((DataTypeValue, DataTypeValue)),
+    Many(Vec<DataTypeValue>)
+}
 
 pub fn recommend(
     magds: &mut MAGDS, 
@@ -13,12 +23,26 @@ pub fn recommend(
     let features: Vec<(u32, DataTypeValue, f32)> = features.into_iter()
         .map(|(id, value)| (*id, value.clone(), 1.0f32))
         .collect();
-    recommend_weighted(magds, &features, target, fuzzy)
+    recommend_weighted(magds, &features, &vec![], target, fuzzy)
+}
+
+pub fn recommend_filter(
+    magds: &mut MAGDS, 
+    features: &[(u32, DataTypeValue)],
+    filters: &[(u32, DataValueFilter)],
+    target: u32,
+    fuzzy: bool
+) -> Option<Vec<(DataTypeValue, f32)>> {
+    let features: Vec<(u32, DataTypeValue, f32)> = features.into_iter()
+        .map(|(id, value)| (*id, value.clone(), 1.0f32))
+        .collect();
+    recommend_weighted(magds, &features, filters, target, fuzzy)
 }
 
 pub fn recommend_weighted(
     magds: &mut MAGDS, 
     features: &Vec<(u32, DataTypeValue, f32)>,
+    filters: &[(u32, DataValueFilter)],
     target: u32,
     fuzzy: bool
 ) -> Option<Vec<(DataTypeValue, f32)>> {
@@ -61,6 +85,9 @@ pub fn recommend_weighted(
     if neurons_len == 0 { return None }
 
     let mut values_sorted: Vec<(DataTypeValue, f32)> = neurons.into_iter()
+        .filter(|neuron| {
+            if filters.is_empty() { true } else { filter_neuron(neuron, filters) }
+        })
         .map(|neuron| (
             neuron.borrow().explain_one(target), 
             if max_activation_sum == 0.0f32 {
@@ -77,8 +104,30 @@ pub fn recommend_weighted(
     Some(values_sorted.into_iter().rev().collect())
 }
 
+fn filter_neuron(
+    neuron: &Rc<RefCell<dyn Neuron>>, filters: &[(u32, DataValueFilter)]
+) -> bool {
+    let neuron = neuron.borrow();
+    for (filter_id, filter) in filters {
+        if let Some(neuron_value) = &neuron.explain_one(*filter_id) {
+            let is_ok = match filter {
+                DataValueFilter::Empty => true,
+                DataValueFilter::One(value) => neuron_value == value,
+                DataValueFilter::Range((min, max)) => {
+                    neuron_value >= min && neuron_value <= max
+                }
+                DataValueFilter::Many(values) => values.contains(neuron_value),
+            };
+            if !is_ok { return false }
+        }
+    }
+    true
+}
+
 #[allow(unused_imports)]
 mod tests {
+    use std::sync::Arc;
+
     use witchnet_common::data::DataTypeValue;
 
     use crate::{
@@ -87,6 +136,8 @@ mod tests {
             algorithm::{ recommendation, prediction }
         }
     };
+
+    use super::*;
 
     #[test]
     fn recommend() {
@@ -113,6 +164,46 @@ mod tests {
         assert!(recommendations.first().unwrap().1 > 0f32);
 
         println!("recommendations {:?}", recommendations);
+
+        let setosa_filters = vec![
+            (variety_sensor_id, DataValueFilter::One(Arc::<str>::from("setosa").into()))
+        ];
+        let recommendations = recommendation::recommend_filter(
+            &mut magds, &features, &setosa_filters, variety_sensor_id, true
+        ).unwrap();
+        assert!(recommendations.len() == 50);
+        assert!(recommendations.first().unwrap().1 > 0f32);
+        println!("recommendations filtered by setosa {:?}", recommendations);
+
+        let empty_filters = vec![(variety_sensor_id, DataValueFilter::Empty)];
+        let recommendations = recommendation::recommend_filter(
+            &mut magds, &features, &empty_filters, variety_sensor_id, true
+        ).unwrap();
+        assert!(recommendations.len() == 150);
+        assert!(recommendations.first().unwrap().1 > 0f32);
+        println!("recommendations filtered by empty filter {:?}", recommendations);
+
+        let range_filters = vec![
+            (sepal_length_sensor_id, DataValueFilter::Range((5.0.into(), 10.0.into())))
+        ];
+        let recommendations = recommendation::recommend_filter(
+            &mut magds, &features, &range_filters, variety_sensor_id, true
+        ).unwrap();
+        assert!(recommendations.len() == 128);
+        assert!(recommendations.first().unwrap().1 > 0f32);
+        println!("recommendations filtered by range filter {:?}", recommendations);
+
+        let many_filters = vec![(
+            variety_sensor_id, DataValueFilter::Many(
+                vec![Arc::<str>::from("setosa").into(), Arc::<str>::from("virginica").into()]
+            )
+        )];
+        let recommendations = recommendation::recommend_filter(
+            &mut magds, &features, &many_filters, variety_sensor_id, true
+        ).unwrap();
+        assert!(recommendations.len() == 100);
+        assert!(recommendations.first().unwrap().1 > 0f32);
+        println!("recommendations filtered by setosa and versicolor {:?}", recommendations);
         
         let pred = prediction::predict(&mut magds, &features, variety_sensor_id, true);
         println!("pred {:?}", pred);
