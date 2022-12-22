c7ebe1b94c14b5e59d4a3c4d26696b754f334e04
danbulnet <daniel@bulanda.net>
2022-12-22 22:28:31 +0100

    refactor sequence_1d resources [wip]

--- crates/witchnet-gui/src/resources/sequence_1d.rs
+++ crates/witchnet-gui/src/resources/sequence_1d.rs
@@ -3,6 +3,12 @@ use std::{
     default::Default
 };
 
+use rand::{ seq::IteratorRandom, thread_rng };
+
+use flex_points::algorithm as fp;
+
+use mint::Point2;
+
 use crate::resources::sequential_data::SequentialDataFile;
 
 type SeriesGenerator = fn() -> Vec<[f64; 2]>;
@@ -12,14 +18,61 @@ type SamplesGenerator = fn(&[[f64; 2]]) -> Vec<[f64; 2]>;
 pub(crate) enum SequenceSelector<'a> {
     ComplexTrigonometric,
     Tanh,
-    LoadedData(&'a SequentialDataFile)
+    LoadedData(&'a SequentialDataFile),
+    None
 }
 
 #[derive(Debug, Clone)]
 pub(crate) enum SamplingMethodSelector {
     FlexPoints,
     RamerDouglasPeucker,
-    Random
+    Random,
+    None
+}
+
+impl SamplingMethodSelector {
+    pub(crate) fn samples(&self, data: &[[f64; 2]]) -> Vec<[f64; 2]> {
+        match self {
+            SamplingMethodSelector::FlexPoints => Self::flex_points(data),
+            SamplingMethodSelector::RamerDouglasPeucker => Self::rdp(data),
+            SamplingMethodSelector::Random => Self::random(data),
+            SamplingMethodSelector::None => vec![],
+        }
+    }
+
+    pub fn random(data: &[[f64; 2]]) -> Vec<[f64; 2]> {
+        let mut rng = thread_rng();
+        data.iter()
+            .choose_multiple(&mut rng, data.len() / 100)
+            .into_iter()
+            .map(|x| *x)
+            .collect()
+    }
+
+    pub fn flex_points(data: &[[f64; 2]]) -> Vec<[f64; 2]> {
+        let x: Vec<f64> = data.into_iter().map(|x| x[0]).collect();
+        let y: Vec<f64> = data.into_iter().map(|x| x[1]).collect();
+
+        let output = fp::flex_points(
+            &x,
+            &y,
+            &[0.0, 0.5, 0.2, 0.0],
+            &[25, 25, 50]
+        );
+
+        output.into_iter().map(|i| [x[i], y[i]]).collect()
+    }
+
+    pub fn rdp(data: &[[f64; 2]]) -> Vec<[f64; 2]> {
+        let x: Vec<f64> = data.into_iter().map(|x| x[0]).collect();
+        let y: Vec<f64> = data.into_iter().map(|x| x[1]).collect();
+
+        let data_points: Vec<Point2<f64>> = data.into_iter().map(|x| Point2::from(*x)).collect();
+
+        ramer_douglas_peucker::rdp(&data_points, 0.05).into_iter()
+            .map(|i| [x[i], y[i]])
+            .collect()
+    }
 }
 
 pub struct Sequence1D {
@@ -28,11 +81,13 @@ pub struct Sequence1D {
     pub loaded_data: Option<Vec<[f64; 2]>>,
     pub data_examples: Vec<(Arc::<str>, SeriesGenerator)>,
 
-    pub selected_sampling_method_name: Option<Arc::<str>>,
-    pub loaded_sampling_method_name: Option<Arc::<str>>,
-    pub loaded_sampling_method: Option<SamplesGenerator>,
-    pub loaded_samples: Option<Vec<[f64; 2]>>,
-    pub sampling_methods: Vec<(Arc::<str>, SamplesGenerator)>,
+    pub selected_sampling_method: SamplingMethodSelector,
+    pub loaded_sampling_method: SamplingMethodSelector,
+    pub loaded_samples: Option<Vec<[f64; 2]>>
+    // pub selected_sampling_method_name: Option<Arc::<str>>,
+    // pub loaded_sampling_method_name: Option<Arc::<str>>,
+    // pub loaded_sampling_method: Option<SamplesGenerator>,
+    // pub sampling_methods: Vec<(Arc::<str>, SamplesGenerator)>,
 }
 
 impl Default for Sequence1D {
@@ -45,32 +100,44 @@ impl Default for Sequence1D {
         examples.push(default_example.clone());
         examples.push(("tanh".into(), examples::tanh));
 
-        let mut sampling_methods = Vec::new();
-        let default_sampling_method: (Arc<str>, SamplesGenerator) = (
-            "flex-points".into(), 
-            sampling::flex_points as fn(&[[f64; 2]]) -> Vec<[f64; 2]>
-        );
-        sampling_methods.push(default_sampling_method.clone());
-        sampling_methods.push(("rdp".into(), sampling::rdp));
-        sampling_methods.push(("random".into(), sampling::random));
-
-        let loaded_name = Some(default_example.0.clone());
-        let loaded_data = Some(default_example.1());
-        let loaded_sampling_method_name = Some(default_sampling_method.0.clone());
-        let loaded_sampling_method = Some(default_sampling_method.1);
-        let loaded_samples = Some(default_sampling_method.1(loaded_data.as_ref().unwrap()));
-
         Sequence1D {
             selected_data_name: loaded_name.clone(),
             loaded_data_name: loaded_name,
             loaded_data, 
             data_examples: examples,
-            selected_sampling_method_name: loaded_sampling_method_name.clone(),
-            loaded_sampling_method_name,
-            loaded_sampling_method,
-            loaded_samples,
-            sampling_methods
+
+            selected_sampling_method: SamplingMethodSelector::FlexPoints,
+            loaded_sampling_method: SamplingMethodSelector::FlexPoints,
+            loaded_samples: SamplingMethodSelector::FlexPoints::samples(
+                loaded_data.as_ref().unwrap()
+            )
         }
+        // let mut sampling_methods = Vec::new();
+        // let default_sampling_method: (Arc<str>, SamplesGenerator) = (
+        //     "flex-points".into(), 
+        //     sampling::flex_points as fn(&[[f64; 2]]) -> Vec<[f64; 2]>
+        // );
+        // sampling_methods.push(default_sampling_method.clone());
+        // sampling_methods.push(("rdp".into(), sampling::rdp));
+        // sampling_methods.push(("random".into(), sampling::random));
+
+        // let loaded_name = Some(default_example.0.clone());
+        // let loaded_data = Some(default_example.1());
+        // let loaded_sampling_method_name = Some(default_sampling_method.0.clone());
+        // let loaded_sampling_method = Some(default_sampling_method.1);
+        // let loaded_samples = Some(default_sampling_method.1(loaded_data.as_ref().unwrap()));
+
+        // Sequence1D {
+        //     selected_data_name: loaded_name.clone(),
+        //     loaded_data_name: loaded_name,
+        //     loaded_data, 
+        //     data_examples: examples,
+        //     selected_sampling_method_name: loaded_sampling_method_name.clone(),
+        //     loaded_sampling_method_name,
+        //     loaded_sampling_method,
+        //     loaded_samples,
+        //     sampling_methods
+        // }
     }
 }
 
