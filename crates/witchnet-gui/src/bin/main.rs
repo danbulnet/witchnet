72aa18532c3a08dac8eb287a0ae3b38f3c6183dc
danbulnet <daniel@bulanda.net>
2023-01-03 01:35:50 +0100

    remove unnecessary code from gui module

--- crates/witchnet-gui/src/bin/main.rs
+++ crates/witchnet-gui/src/bin/main.rs
@@ -3,7 +3,7 @@ use std::env;
 use witchnet_gui::interface::app;
 
 fn main() {
-    let args: Vec<String> = std::env::args().skip(1).collect();
+    let args: Vec<String> = env::args().skip(1).collect();
 
     app::app(args)
 }
