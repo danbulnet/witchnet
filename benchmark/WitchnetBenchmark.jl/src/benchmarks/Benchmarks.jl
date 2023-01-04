7e78d8fde77703fc074a8c4326fbd7427e020ae4
danbulnet <daniel@bulanda.net>
2023-01-04 19:34:55 +0100

    add summary of datasets benchmark fn

--- benchmark/WitchnetBenchmark.jl/src/benchmarks/Benchmarks.jl
+++ benchmark/WitchnetBenchmark.jl/src/benchmarks/Benchmarks.jl
@@ -1,4 +1,4 @@
-export warmup, predictall
+export warmup, predictall, classifyall, estimateall, summarizeall
 
 include("Iris.jl")
 include("Penguin.jl")
@@ -19,15 +19,72 @@ end
 function predictall(
     classifymodels=fast_classification_models(),
     estimatemodels=fast_regression_models()
-)
-    Iris.classify(models=classifymodels)
-    Iris.estimate(models=estimatemodels)
-    Penguin.classify(models=classifymodels)
-    Penguin.estimate(models=estimatemodels)
-    Star.classify(models=classifymodels)
-    Star.estimate(models=estimatemodels)
-    WhiteWine.classify(models=classifymodels)
-    WhiteWine.estimate(models=estimatemodels)
-    RedWine.classify(models=classifymodels)
-    RedWine.estimate(models=estimatemodels)
+)::Dict{Symbol, Dict{Symbol, DataFrame}}
+    Dict(
+        :classification => classifyall(classifymodels),
+        :regression => estimateall(estimatemodels)
+    )
+end
+
+function classifyall(classifymodels=fast_classification_models())::Dict{Symbol, DataFrame}
+    Dict(
+        :iris => Iris.classify(models=classifymodels),
+        :penguin => Penguin.classify(models=classifymodels),
+        :star => Star.classify(models=classifymodels),
+        :whitewine => WhiteWine.classify(models=classifymodels),
+        :redwine => RedWine.classify(models=classifymodels)
+    )
+end
+
+function estimateall(estimatemodels=fast_classification_models())::Dict{Symbol, DataFrame}
+    Dict(        
+        :iris => Iris.estimate(models=estimatemodels),
+        :penguin => Penguin.estimate(models=estimatemodels),
+        :star => Star.estimate(models=estimatemodels),
+        :whitewine => WhiteWine.estimate(models=estimatemodels),
+        :redwine => RedWine.estimate(models=estimatemodels)
+    )
+end
+
+function summarizeall(results=predictall())::Dict{Symbol, DataFrame}
+    classification = results[:classification]
+    regression = results[:regression]
+
+    classificationlen = length(values(classification))
+    regressionlen = length(values(regression))
+
+    accuracy = sum(getproperty.(values(classification), :accuracy)) / classificationlen
+    time = sum(getproperty.(values(classification), :time)) / classificationlen
+    memory = sum(getproperty.(values(classification), :memory)) / classificationlen
+    classificationmean = DataFrame(
+        :model => first(values(classification)).model,
+        :accuracy => accuracy,
+        :time => time,
+        :memory => memory
+    )
+
+    rmse = sum(getproperty.(values(regression), :rmse)) / regressionlen
+    time = sum(getproperty.(values(regression), :time)) / regressionlen
+    memory = sum(getproperty.(values(regression), :memory)) / regressionlen
+    regressionmean = DataFrame(
+        :model => first(values(regression)).model,
+        :rmse => rmse,
+        :time => time,
+        :memory => memory
+    )
+
+    Utils.writecsv(classificationmean, "summary", "classification", :mean)    
+    title = string("classification accuracy mean of ", classificationlen, " datasets")
+    plot = percent_barplot(classificationmean, :model, :accuracy, title)
+    Utils.writeimg(plot, "summary", "accuracy", :mean)
+
+    Utils.writecsv(regressionmean, "summary", "regression", :mean)
+    title = string("regression rmse mean of ", regressionlen, " datasets")
+    plot = value_barplot(regressionmean, :model, :rmse, title)
+    Utils.writeimg(plot, "summary", "rmse", :mean)
+
+    Dict(
+        :classificationmean => classificationmean,
+        :regressionmean => regressionmean
+    )
 end
